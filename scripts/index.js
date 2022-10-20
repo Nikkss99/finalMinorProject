3df667fdb065dae278e8dc59cc2b85a3e4ab5c41
Cherub <tmajmer@gmail.com>
2022-10-21 01:47:46 +0530

    login added

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
-
+const login = document.getElementById("loginbtn")
+const loginPage= document.getElementById("login")
 const submitBtn = document.querySelectorAll(".loginButton");
 const button = document.getElementById("explore");
 const explorebutton = document.getElementById("expore2")
@@ -6,7 +7,9 @@ const link1 = document.getElementById("link")
 const shopnow = document.getElementsByClassName("rightHero")
 const popup = document.getElementsByClassName("loginform");
 const imagearray = ["./resources/miniproject/Rectangle 4.png","./resources/miniproject/Rectangle 2.png","./resources/miniproject/Rectangle 5.png"]
-console.log(link1)
+login.addEventListener("click",()=>{
+    loginPage.click();
+})
 for(var i =1;i<submitBtn.length;i++){
     submitBtn[i].addEventListener("click",()=>{
         link1.click()
@@ -21,9 +24,6 @@ button.addEventListener("click",()=>{
 })
 explorebutton.addEventListener("click",()=>{
     link1.click();
-})
-console.log(shopnow[0].lastElementChild)   
-shopnow[0].lastElementChild[0].addEventListener("click",()=>{
-    
-    link1.click();
-})
+})   
+
+

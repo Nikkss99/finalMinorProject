57596cc48c469dc5ce5b7026bb4c25af17bb2048
Cherub <tmajmer@gmail.com>
2022-10-21 01:21:32 +0530

    done

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,21 @@
-const loginButton = document.getElementById("loginbtn");
-const submitBtn = document.getElementsByClassName("login");
+
+const submitBtn = document.querySelectorAll(".loginButton");
 const button = document.getElementById("explore");
 const explorebutton = document.getElementById("expore2")
 const link1 = document.getElementById("link")
 const shopnow = document.getElementsByClassName("rightHero")
 const popup = document.getElementsByClassName("loginform");
 const imagearray = ["./resources/miniproject/Rectangle 4.png","./resources/miniproject/Rectangle 2.png","./resources/miniproject/Rectangle 5.png"]
-console.log(submitBtn[0])
-loginButton.addEventListener("click",()=>{
-    console.log();
-    submitBtn[0].click();
-})
+console.log(link1)
+for(var i =1;i<submitBtn.length;i++){
+    submitBtn[i].addEventListener("click",()=>{
+        link1.click()
+    })
+}
 
-submitBtn.addEventListener("click",()=>{
-    popup[0].style.display = "none"
-})
+navigate=()=>{
+    link1.click()
+}
 button.addEventListener("click",()=>{
     link1.click();
 })
